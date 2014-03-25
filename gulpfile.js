23316d7622034940c7a7a796510631eefc0321c9
Crowther, Josh <jshcrowthe@gmail.com>
2014-03-25 09:42:59 -0600

    Updated Gulp File (Personal Preferances, sorry) Modified Event Type Landing Page to latest Mockup

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var minify = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var notify = require('gulp-notify');
 
 gulp.task('default', ['less', 'minifycss'], function() {
 	gulp.watch('./*.less', ['less', 'minifycss']);
@@ -11,7 +12,8 @@ gulp.task('less', function() {
 	return gulp.src('./*.less')
 	.pipe(less())
 	.pipe(rename("style.css"))
-	.pipe(gulp.dest("./"));
+	.pipe(gulp.dest("./"))
+	.pipe(notify('LESS Compiled Succesfully'));;
 });
 
 gulp.task('minifycss', function() {
