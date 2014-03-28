8ab39add903830b9a7f641bde2a0d20ef85e6ba9
Matthew Sessions <hyphrend@matthew.(none)>
2014-03-28 15:01:25 -0600

    Updated home page to match new mock up.

--- home.php
+++ home.php
@@ -1,102 +1,103 @@
+<?php
+//get the most popular speeches
+$popularLoop = new WP_Query(array("post_type" => array("devotional", "forum"), "orderby" => "views", "posts_per_page" => "5"));
+?>
+
 <?php get_header(); ?>
 
 <div class="row">
-    <div class="col-xs-12 col-sm-8">
+    <div class="col-xs-12 home-carousel">
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
-            <?php 
-            $loop = new WP_Query(array(
-                    'post_type' => 'devotional',
-                    "meta_query" => array(
-                        "key" => "even_date",
-                        "value" => strtotime('now'),
-                        "compare" => ">",
-                        "type" => "DATE"
-                    )
-                ));
-            ?>
             <ol class="carousel-indicators">
-                <?php
-                $counter = 0;
-                while($loop->have_posts()) :
-                    $loop->the_post();
-                    $active = ($counter == 0) ? "class='active'" : "";
-                    ?>
-                    <li data-target="#carousel-example-generic" data-slide-to="<?php echo $counter; ?>" <?php echo $active; ?>></li>
-                    <?php
-                    $counter++;
-                endwhile;
-                ?>
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
             </ol>
 
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
-                <?php
-                $counter = 0;
-                while ($loop->have_posts()) :
-                    $loop->the_post();
-                    $meta = get_post_meta(get_the_ID());
-
-                    $active = ($counter == 0) ? "active" : "";
-                    $url = get_permalink();
-                    $date = date('F j, Y', strtotime($meta['event_date'][0]));
-                    $speakerId = explode(", ", $meta['presenters'][0]);
-                    $speakerName = get_the_title($speakerId[0]);
-                    $speakerTitle = get_post_meta($speakerId, "title");
-                    $image = wp_get_attachment_image_src(get_post_thumbnail_id($speakerId));
-                    ?>
-                    <div class="item <?php echo $active ?>">
-                        <div class="home-carousel group">
-                            <img class="carousel-image" src="<?php echo $image[0]; ?>" />
-                            <div class="carousel-info">
-                                <p class="carousel-speaker-name"><?php echo $speakerName; ?></p>
-                                <p class="carousel-speaker-title"><?php echo $speakerTitle[0]; ?></p>
-                                <p class="carousel-date"><?php echo $date; ?></p>
-                                <a class="carousel-link" href="<?php echo $url; ?>">How To Prepare</a>
-                            </div>
-                        </div>
-                    </div>
-                    <?php
-                    $counter++;
-                endwhile;
-                ?>
-            </div>
-        </div>
-        <div class="recent-speeches">
-            <h2>Recent Speeches</h2>
-            <?php
-            $counter = 0;
-            $loop = new WP_Query(array('post_type' => 'devotional'));
-            while ($loop->have_posts() && $counter < 4) : $loop->the_post();
-                $meta = get_post_meta(get_the_ID());
-                date_default_timezone_set('America/Denver');
-                $now = strtotime('now');
-                $post_end_time = get_post_meta(get_the_ID(), 'event_end_time');
-                if ($now > $post_end_time[0]) :
-                    ?>
-
-                    <div class="recent-speech group">
-                        <div class="speaker-pic">
-                            <?php echo get_the_post_thumbnail($meta['presenters'][0]); ?>
-                        </div>
-                        <div class="speaker-info">
-                            <a href="<?php the_permalink(); ?>">
-                                <h4>"<?php the_title(); ?>"</h4>
-                            </a>
-                            <p><?php echo get_the_title($meta['presenters'][0]); ?></p>
-                            <span class="post-date"><?php echo date('F jS, Y', strtotime($meta['event_date'][0])); ?></span>
-                        </div>
-                    </div>
-                    <?php
-                    $counter++;
-                endif;
-            endwhile;
-            ?>
-            <div class="center">
-                <a class="speeches-button">View All Speeches</a>
+                <div class="item active">
+                    <img class="carousel-image" src="/wp-content/themes/speeches-theme/images/home/devotionals.jpg" />
+                </div>
+                <div class="item">
+                    <img class="carousel-image" src="/wp-content/themes/speeches-theme/images/home/usher.jpg" />
+                </div>
+                <div class="item">
+                    <img class="carousel-image" src="/wp-content/themes/speeches-theme/images/home/forum.jpg" />
+                </div>
+                <div class="item">
+                    <img class="carousel-image" src="/wp-content/themes/speeches-theme/images/home/QA.jpg" />
+                </div>
             </div>
         </div>
     </div>
-    <?php get_sidebar(); ?>
+</div>
+<div class="row">
+    <div class="col-xs-12 col-md-4">
+        <div class="featured-topic speech-box">
+            <h3>Featured Topic</h3>
+            <div>
+                <p class="featured-topic-text">Faith</p>
+                <p class="featured-topic-description">This month we are highlighting past speeches on faith. Consider reading, watching, or listening to these talks for your personal study.</p>
+                <div class="popular-speech">
+                    <a href="#">
+                        <p class="featured-title">The Title</p>
+                        <p class="featured-speaker">Speaker Name</p>
+                    </a>
+                </div>
+                <div class="featured-speech">
+                    <a href="#">
+                        <p class="featured-title">The Title</p>
+                        <p class="featured-speaker">Speaker Name</p>
+                    </a>
+                </div>
+                <div class="featured-speech">
+                    <a href="#">
+                        <p class="featured-title">The Title</p>
+                        <p class="featured-speaker">Speaker Name</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-4">
+        <div class="popular-speeches speech-box">
+            <h3>Popular Speeches</h3>
+            <div>
+                <?php
+                while ($popularLoop->have_posts()) :
+                    $popularLoop->the_post();
+                    $speakers = explode(", ", get_post_meta(get_the_ID(), "presenters", true));
+                    $speaker_text = "";
+                    $numSpeakers = count($speakers);
+                    for ($i = 0; $i < $numSpeakers; $i++) {
+                        if ($i != 0 && $numSpeakers > 2) {
+                            $speaker_text .= ",";
+                        }
+                        if ($i + 1 == $numSpeakers && $numSpeakers > 1) {
+                            $speaker_text .= " and";
+                        }
+                        $speaker_text .= get_the_title($speakers[$i]);
+                    }
+                    ?>
+                    <div class="popular-speech">
+                        <a href="<?php echo the_permalink(); ?>">
+                            <p class="popular-title"><?php echo get_the_title(); ?></p>
+                            <p class="popular-speaker"><?php echo $speaker_text; ?></p>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-4">
+        <div class="home-page-links">
+            <a href="/devotionals" class="devotionals-link">Devotional Home ></a>
+            <a href="/forums" class="forums-link">University Forums Home ></a>
+            <a href="/archive" class="archive-link">Speeches Archive ></a>
+        </div>
+    </div>
 </div>
 <?php get_footer(); ?>
