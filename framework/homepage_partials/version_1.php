effe4b903a7a4e6797924b08dcd0b90dba30d462
Brandyn Burbank <bur11012@byui.edu>
2014-06-16 17:05:40 -0600

    continued work on homepage v1: finished final hero banner tweaks (cta buttons etc) started work on archive search field.

--- framework/homepage_partials/version_1.php
+++ framework/homepage_partials/version_1.php
@@ -5,12 +5,34 @@
 			<h2 class="cap-title">This Week's Devotional</h2>
 			<div class="cap-desc">Bacon ipsum dolor sit amet voluptate cillum fatback culpa deserunt bresaola pork tempor pancetta ea capicola. Jerky cow short ribs ball tip adipisicing. Ut doner hamburger rump aute reprehent&nbsp;laborum.</div>
 			<div class="cap-more"><a href="#">See More &gt;&gt;</a></div>
+			<div class="cap-cta"><a href="#" class="cta">Join</a> <a href="#" class="cta">Join</a> <a href="#" class="cta">Join</a></div>
 		</div>
-		<div class="cap-cta"><div class="cta">Join</div><div class="cta">Join</div><div class="cta">Join</div></div>
 	</div>
 	<a class="rotate-left">&lt;</a>
 	<a class="rotate-right">&gt;</a>
 </div>
 
 <div class="container content-wrap">
+
+<form id="master-search-form" class="master-search" action="javascript:void();">
+		<div class="search-icon"></div>
+		<input type="text" placeholder="Search the Archive" name="query" autocomplete="off" class="master-query" id="search-query" />
+	<div class="search-dropdown">
+		<div class="dd-header">Did you mean?</div>
+		<div id="ajax-suggestions" class="search-suggestions">
+			<a href="#" class="ajax-result">Presenter &mdash; Elder D. Todd Christofferson - 10 Speeches</a>
+		</div>
+		<div class="dd-header">Additional Options:</div>
+		<div class="additional-options">
+			<input checked="" type="checkbox" name="event" value="devotional" id="devotional">
+			<label for="devotional">Search Devotionals</label>
+			<input checked="" type="checkbox" name="event" value="forum" id="forum">
+			<label for="forum">Search University Devotionals</label>
+			<input checked="" type="checkbox" name="event" value="other" id="other">
+			<label for="other">Search Other Speeches</label>
+			<input id="submit" type="submit" value="Search" />
+		</div>
+	</div>
+</form>
+
 </div>
