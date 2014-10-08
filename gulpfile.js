0b0084147ec485b19c1238b25064b216b4d0e664
Matthew Sessions <shichongrui@gmail.com>
2014-10-08 09:15:13 -0600

    Got plumber working and image minification

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,14 @@ var gulp = require('gulp'),
     uglify = require("gulp-uglify"),
     replace = require("gulp-replace"),
     flatten = require("gulp-semiflat"),
+    plumber = require("gulp-plumber"),
+    imagemin = require("gulp-imagemin"),
     exec = require("child_process").exec,
     fs = require("fs");
 
 gulp.task("copy", function () {
     return gulp.src(["./code/**/*", "!./**/*.less"])
+        .pipe(plumber())
         .pipe(copy("./dist", {
             prefix: 1
         }));
@@ -36,6 +39,7 @@ gulp.task("insert", ["insert:header", "insert:footer"]);
 
 gulp.task("insert:header", function () {
     return gulp.src(["./code/header.php"])
+        .pipe(plumber())
         .pipe(gfi({
             "<!-- header.html -->": "./code/assets/html/header.html"
         }))
@@ -46,6 +50,7 @@ gulp.task("insert:header", function () {
 
 gulp.task("insert:footer", function () {
     return gulp.src(["./code/footer.php"])
+        .pipe(plumber())
         .pipe(gfi({
             "<!-- footer.html -->": "./code/assets/html/footer.html"
         }))
@@ -54,6 +59,7 @@ gulp.task("insert:footer", function () {
 
 gulp.task('less', function () {
     return gulp.src('./code/assets/css/style.less')
+        .pipe(plumber())
         .pipe(less())
         .pipe(rename("style.css"))
         .pipe(gulp.dest("./dist/"))
@@ -62,6 +68,7 @@ gulp.task('less', function () {
 
 gulp.task('minifycss', function () {
     return gulp.src(['./dist/**/*.css', '!./dist/style.css', "!./dist/assets/css/global.min.css"])
+        .pipe(plumber())
         .pipe(flatten("./dist/assets/css"))
         .pipe(minify())
         .pipe(gulp.dest("./dist/assets/css/"));
@@ -69,16 +76,27 @@ gulp.task('minifycss', function () {
 
 gulp.task("minifystyle", ["less"], function () {
     return gulp.src(["./dist/style.css"])
+        .pipe(plumber())
         .pipe(minify())
         .pipe(gulp.dest("./dist/"));
 });
 
 gulp.task('minifyjs', function () {
     return gulp.src(['./code/assets/js/admin/*.js'])
+        .pipe(plumber())
         .pipe(concat("speechesjs.min.js"))
         .pipe(uglify())
         .pipe(gulp.dest('./dist/assets/js/'))
         .pipe(notify('Javascript Compiled Succesfully'));
+});
+
+gulp.task("imagemin", function () {
+    return gulp.src("./dist/assets/images/**")
+        .pipe(plumber())
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest("./dist/assets/images"))
 });
 
 gulp.task('default', ['insert', 'copy', 'less', 'minifycss', "minifystyle", 'minifyjs'], function () {
