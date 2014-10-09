be1f2b8afc340c5c9a74e37c7d0f34633bbe60e2
Matthew Sessions <shichongrui@gmail.com>
2014-10-09 16:55:35 -0600

    Broke somewhat out into partials

---
+++ code/partials/archive/filter-box.php
@@ -0,0 +1,15 @@
+<div class="filter-box">
+    <h3>EVENT TYPE</h3>
+    <div class="filters">
+        <div class="flat-form-elements">
+            <input checked type="checkbox" name="event" value="devotional" id="devotional">
+            <label for="devotional">Devotionals</label>
+            <br>
+            <input type="checkbox" name="event" value="forum" id="forum">
+            <label for="forum">University Forums</label>
+            <br>
+            <input type="checkbox" name="event" value="graduation" id="graduation">
+            <label for="graduation">Graduation</label>
+        </div>
+    </div>
+</div>
